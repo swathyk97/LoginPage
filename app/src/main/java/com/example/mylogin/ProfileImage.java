6787c648a4dc4e115579ed5fac1cf8f216726d87
Swathy k s <swathy.k@gadgeon.com>
2020-09-18 19:02:46 +0530

    review comment code changes2

--- app/src/main/java/com/example/mylogin/ProfileImage.java
+++ app/src/main/java/com/example/mylogin/ProfileImage.java
@@ -1,7 +1,11 @@
 package com.example.mylogin;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -9,10 +13,16 @@ import android.widget.ImageView;
 
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.io.IOException;
 
 public class ProfileImage extends Activity {
     Button btnImage;
-    ImageView imageView;
+   CircularImageView imageView;
+   private static final int PICK_IMAGE=1;
+   Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +33,31 @@ public class ProfileImage extends Activity {
         btnImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Glide.with(getApplicationContext())
-                        .load(R.drawable.flower)
-                        .into(imageView);
+                Intent gallery=new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(gallery,"select picture"),PICK_IMAGE);
+
 
             }
         });
 
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode== PICK_IMAGE && resultCode == RESULT_OK){
+            imageUri=data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 }
